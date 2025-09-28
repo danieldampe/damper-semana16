da435ef24b0b959568bc7447f59a7fbfcf73a508
danieldampe <damper.daniel.sm@gmail.com>
2025-09-28 00:24:53 -0500

    removing react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,8 @@
 import './App.css'
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import { Home } from './pages/home/Home';
-import { Contact } from './pages/contact/Contact';
 import { Post } from './pages/post/Post';
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        {/*  <nav>
-                    <Link to="/">Home </Link>
-                    <Link to="/post">Post </Link>
-                    <Link to="/contact">Contact </Link>
-
-                </nav> */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/post" element={<Post />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+  return <Post />
 }
 
 export default App
